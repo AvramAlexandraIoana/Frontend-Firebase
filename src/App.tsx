58eb9e8a97ee10a9ce27e0b085bb5b673b375572
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2023-12-04 14:18:43 +0200

    worked

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
+import Login from './components/Auth/Login';
 
 function App() {
   return (
