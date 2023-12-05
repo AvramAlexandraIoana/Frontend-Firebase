28d4e50de77b2d200bad15385dc2576683d33483
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2023-12-05 15:58:30 +0200

    worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
 import RegisterFormFacade from './components/Register/RegisterFormFacade';
+import LoginFormFacade from './components/Login/LoginFormFacade';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginFormFacade />} />
         <Route path="/register" element={<RegisterFormFacade />} />
+        <Route path="/login" element={<LoginFormFacade />} />
       </Routes>
     </>
   );
---
+++ src/components/Login/LoginFormFacade.tsx
@@ -0,0 +1,84 @@
+// LoginFormFacade.tsx
+import React, { useState } from 'react';
+import { LockOutlined } from '@mui/icons-material';
+import { Box, Avatar, Typography, Grid } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import FormBuilder from '../FormBuilder/FormBuilder';
+import { createAvatar, createButton, createMuiLink, createTypography } from '../ComponentFactory/ComponentFactory';
+import adaptToLayout from '../Adapter/Adapter';
+import withLayout from '../withLayout/withLayout';
+
+const LoginFormFacade = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const formBuilder = new FormBuilder();
+
+  const handleLogin = () => {
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  const form = formBuilder
+    .addTextField({
+      label: 'Email Address',
+      name: 'email',
+      value: email,
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value),
+      validators: ['required', 'isEmail'],
+      errorMessages: ['This field is required', 'Email is not valid'],
+    })
+    .addTextField({
+      label: 'Password',
+      name: 'password',
+      value: password,
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value),
+      type: 'password',
+      showPassword: showPassword,
+      onChangeToggle: () => setShowPassword(!showPassword),
+      validators: [
+        'required',
+        'minStringLength:8',
+        'maxStringLength:20',
+        'matchRegexp:^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).*$',
+      ],
+      errorMessages: [
+        'This field is required',
+        'Password must be at least 8 characters long',
+        'Password must be at most 20 characters long',
+        'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character',
+      ],
+    })
+    .buildForm(handleLogin);
+
+  return adaptToLayout(
+    <>
+      {createAvatar({ sx: { m: 'auto', mb: 1, bgcolor: 'primary.light' }, children: <LockOutlined /> })}
+      {createTypography({ variant: 'h5', children: 'Login' })}
+      {form}
+      {createButton({
+        type: 'submit',
+        fullWidth: true,
+        variant: 'contained',
+        sx: { mt: 3, mb: 2, borderRadius: 20 },
+        children: 'Login',
+        onClick: handleLogin,
+      })}
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+          {createMuiLink({
+            component: RouterLink,
+            to: '/login',
+            variant: 'body2',
+            color: 'primary',
+            underline: 'hover',
+            children: "Don't have an account? Register",
+          })}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default withLayout(LoginFormFacade);
