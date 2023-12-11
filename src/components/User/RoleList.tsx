822da8b2cc75bda9ab5557e8d09cbdaeeacfd5b1
Your Name <ioana-alexandra.avram@my.fmi.unibuc.ro>
2023-12-11 23:20:44 +0200

    worked

--- src/components/User/RoleList.tsx
+++ src/components/User/RoleList.tsx
@@ -1,46 +1,91 @@
 import React, { useEffect, useState } from 'react';
-// import MaterialTable from 'material-table';
+import MaterialTable from 'material-table';
 import { AuthService } from '../../services/Auth/AuthService';
 import { Role } from '../../interfaces/Auth/Role';
 import CustomAppBar from '../AppBar/CustomAppBar';
+import { Card, CardContent, Container } from '@mui/material';
 
-const RoleList = () => {
+// Import Material-UI icons
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import CheckIcon from '@material-ui/icons/Check';
+import ClearIcon from '@material-ui/icons/Clear';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import EditIcon from '@material-ui/icons/Edit';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import RemoveIcon from '@material-ui/icons/Remove';
+import SearchIcon from '@material-ui/icons/Search';
+import ViewColumnIcon from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+  Add: AddBoxIcon,
+  Check: CheckIcon,
+  Clear: ClearIcon,
+  Delete: DeleteOutlineIcon,
+  DetailPanel: ChevronRightIcon,
+  Edit: EditIcon,
+  Export: SaveAltIcon,
+  Filter: FilterListIcon,
+  FirstPage: FirstPageIcon,
+  LastPage: LastPageIcon,
+  NextPage: ChevronRightIcon,
+  PreviousPage: ChevronLeftIcon,
+  ResetSearch: ClearIcon,
+  Search: SearchIcon,
+  SortArrow: ChevronRightIcon,
+  ThirdStateCheck: RemoveIcon,
+  ViewColumn: ViewColumnIcon,
+};
+
+const RoleList: React.FC = () => {
   const [roleList, setRoleList] = useState<Role[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const authService = new AuthService();
 
   useEffect(() => {
-    // Fetch the role list when the component mounts
     fetchRoleList();
   }, []);
 
   const fetchRoleList = async () => {
     try {
+      setIsLoading(true);
       const roles = await authService.getRoleList();
-      console.log('roles', roles);
       setRoleList(roles);
     } catch (error) {
       console.error('Error fetching role list:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const columns = [
-    { title: 'Role ID', field: 'roleId' },
-    { title: 'Role Name', field: 'roleName' },
+    { title: 'Role Name', field: 'name' },
     // Add more columns based on your Role object properties
   ];
 
   return (
-    <CustomAppBar />
-    // <MaterialTable
-    //   title="Role List"
-    //   columns={columns}
-    //   data={roleList}
-    //   options={{
-    //     pageSize: 10,
-    //     pageSizeOptions: [5, 10, 20],
-    //     search: false,
-    //   }}
-    // />
+    <>
+      <CustomAppBar />
+        <div style={{ margin: '20px' }}>
+          <MaterialTable
+            title="Role List"
+            columns={columns}
+            data={roleList}
+            icons={tableIcons as any}
+            isLoading={isLoading}
+            options={{
+              pageSize: 10,
+              pageSizeOptions: [10],
+              search: true,
+              sorting: false,
+            }}
+          />
+        </div>
+    </>
   );
 };
 
